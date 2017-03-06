c16de161400f2d1ffa56d5041839c3368528debd
sairoutine <sairoutine@gmail.com>
2017-03-06 23:57:20 +0900

    add: loading scene

--- src/js/game.js
+++ src/js/game.js
@@ -2,6 +2,8 @@
 var core = require('./hakurei').core;
 var util = require('./hakurei').util;
 
+var LoadingScene = require('./scene/loading');
+
 var Game = function(canvas) {
 	core.apply(this, arguments);
 };
@@ -9,6 +11,9 @@ util.inherit(Game, core);
 
 Game.prototype.init = function () {
 	core.prototype.init.apply(this, arguments);
+
+	this.addScene("loading", new LoadingScene(this));
+	this.changeScene("loading");
 
 };
 
---
+++ src/js/scene/loading.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// scene to load image and sound
+
+var base_scene = require('../hakurei').scene.base;
+var util = require('../hakurei').util;
+
+var SceneLoading = function(core) {
+	base_scene.apply(this, arguments);
+};
+util.inherit(SceneLoading, base_scene);
+
+SceneLoading.prototype.init = function() {
+	base_scene.prototype.init.apply(this, arguments);
+	this.core.image_loader.loadImage("block", "./image/block.png");
+};
+
+SceneLoading.prototype.beforeDraw = function() {
+	base_scene.prototype.beforeDraw.apply(this, arguments);
+
+	console.log(this.core.image_loader);
+	if (this.core.image_loader.isAllLoaded()) {
+		this.core.changeScene("title");
+	}
+};
+SceneLoading.prototype.draw = function(){
+	base_scene.prototype.draw.apply(this, arguments);
+
+	// TODO: update loading message
+	var ctx = this.core.ctx;
+	ctx.save();
+	ctx.fillStyle = 'rgb( 0, 0, 0 )';
+	ctx.textAlign = 'right';
+	ctx.font = "30px 'ＭＳ ゴシック'";
+	ctx.fillText('Now Loading...', 400, 225);
+	ctx.restore();
+};
+
+module.exports = SceneLoading;
