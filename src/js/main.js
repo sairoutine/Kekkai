f5032bbb9a5724cdae08d8df0d92e24b4d266125
sairoutine <sairoutine@gmail.com>
2017-03-06 23:37:57 +0900

    add: game system

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,60 @@
+'use strict';
+var Game = require('./game');
 
+// WebAudio
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+var game;
+
+window.onload = function() {
+	// Canvas
+	var mainCanvas = document.getElementById('mainCanvas');
+	// Game オブジェクト
+	game = new Game(mainCanvas);
+	// 初期化
+	game.init();
+	// キーバインド
+	window.onkeydown = function(e) { game.handleKeyDown(e); };
+	window.onkeyup   = function(e) { game.handleKeyUp(e); };
+
+	// ゲームパッド
+	if(window.Gamepad && navigator.getGamepads) {
+		game.enableGamePad();
+	}
+
+	// ゲーム起動
+	game.startRun();
+};
+window.onerror = function (msg, file, line, column, err) {
+	/*
+	msg: error message
+	file: file path
+	line: row number
+	column: column number
+	err: error object
+	*/ 
+	window.alert(msg + "\n" + line + ":" + column);
+};
+/*
+window.runGame = function () {
+	game.startRun();
+};
+window.stopGame = function () {
+	game.stopRun();
+};
+window.changeFullScreen = function () {
+	var mainCanvas = document.getElementById('mainCanvas');
+	if (mainCanvas.requestFullscreen) {
+		mainCanvas.requestFullscreen();
+	}
+	else if (mainCanvas.msRequestuestFullscreen) {
+		mainCanvas.msRequestuestFullscreen();
+	}
+	else if (mainCanvas.mozRequestFullScreen) {
+		mainCanvas.mozRequestFullScreen();
+	}
+	else if (mainCanvas.webkitRequestFullscreen) {
+		mainCanvas.webkitRequestFullscreen();
+	}
+};
+*/
