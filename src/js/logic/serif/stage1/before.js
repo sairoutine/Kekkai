a892cd740ec548c78b4f0f8de66af60c3e2e9364
sairoutine <sairoutine@gmail.com>
2017-03-11 23:12:24 +0900

    update: serif

--- src/js/logic/serif/stage1/before.js
+++ src/js/logic/serif/stage1/before.js
@@ -2,9 +2,20 @@
 
 // セリフ
 var Serif= [
-	{"pos":"left","exp":"normal","chara":"reimu","fukidashi":"normal","serif":"ここはどこかしら"},
+	{"pos":"left","exp":"normal","chara":"reimu","fukidashi":"normal","serif":"いたた..."},
 	{"pos":"right","exp":"laugh","chara":"yukari","fukidashi":"normal","serif":"あらあら"},
-	{"pos":"left","exp":"normal","chara":"reimu","fukidashi":"normal","serif":"あ、ゆかりじゃないの"},
-	{"pos":"right","exp":"laugh","chara":"yukari","fukidashi":"normal","serif":"どうしたのかしら"},
+	{"pos":"left","exp":"normal","chara":"reimu","fukidashi":"normal","serif":"あ、ゆかり"},
+	{"pos":"left","exp":"angry","chara":"reimu","fukidashi":"normal","serif":"またアンタのしわざね"},
+	{"pos":"right","exp":"laugh","chara":"yukari","fukidashi":"normal","serif":"暇そうにしてたから♪"},
+	{"pos":"right","exp":"laugh","chara":"yukari","fukidashi":"normal","serif":"それよりほら、見て"},
+	{"pos":"left","exp":"angry","chara":"reimu","fukidashi":"normal","serif":"なによ"},
+	{"pos":"left","exp":"laugh2","chara":"reimu","fukidashi":"normal","serif":"あ、お金！"},
+	{"pos":"right","exp":"laugh","chara":"yukari","fukidashi":"normal","serif":"せっかくだから、拾って帰りましょう"},
+	{"pos":"left","exp":"normal","chara":"reimu","fukidashi":"normal","serif":"取れない場所にもあるわ"},
+	{"pos":"right","exp":"laugh","chara":"yukari","fukidashi":"normal","serif":"わたしも手伝ってあげるわよ"},
+	{"pos":"right","exp":"laugh","chara":"yukari","fukidashi":"normal","serif":"Zボタンで私と霊夢の位置を入れかえることができるわ"},
+	{"pos":"right","exp":"laugh","chara":"yukari","fukidashi":"normal","serif":"私は霊夢と反対の位置にいるから"},
+	{"pos":"left","exp":"normal","chara":"reimu","fukidashi":"normal","serif":"Xボタンね"},
+	{"pos":"right","exp":"laugh","chara":"yukari","fukidashi":"normal","serif":"ステージにあるお金を全部取れたら、帰れるわよ"},
 ];
 module.exports = Serif;
