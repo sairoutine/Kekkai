77efb79b016a6a4f591ef71cb2909e0a81b64ac7
sairoutine <sairoutine@gmail.com>
2017-05-05 19:51:58 +0900

    fix: serif of stage2

--- src/js/logic/serif/stage2/before.js
+++ src/js/logic/serif/stage2/before.js
@@ -7,5 +7,9 @@ var Serif= [
 	{"pos":"left","exp":"normal1","chara":"yukari","fukidashi":"normal","serif":"まだ先があるみたいね"},
 	{"pos":"left","exp":"normal1","chara":"yukari","fukidashi":"normal","serif":"敵みたいなのがいるから気をつけて"},
 	{"pos":"right","exp":"normal1","chara":"reimu","fukidashi":"normal","serif":"はいはい"},
+	{"pos":"left","exp":"normal1","chara":"yukari","fukidashi":"normal","serif":"わたしは敵に触れても大丈夫なのよ"},
+	{"pos":"right","exp":"normal1","chara":"reimu","fukidashi":"normal","serif":"はいはい"},
+	{"pos":"left","exp":"normal1","chara":"yukari","fukidashi":"normal","serif":"霊夢はあのオバケみたいなのに触れちゃだめよ"},
+	{"pos":"right","exp":"angry1","chara":"reimu","fukidashi":"normal","serif":"アンタだけずるくない？！"},
 ];
 module.exports = Serif;
