18051ba82dd95ef8a1f43ebe388572e9b1cecfc0
sairoutine <sairoutine@gmail.com>
2017-05-06 02:25:17 +0900

    update: to animate block disappear

--- src/js/object/tile/block_brown.js
+++ src/js/object/tile/block_brown.js
@@ -1,4 +1,12 @@
 'use strict';
+
+/* 乗ると消えるブロック */
+
+
+// 消えるまでの時間
+var FALL_SPAN = 20;
+
+
 var CONSTANT = require('../../constant');
 var base_object = require('./block_base');
 var util = require('../../hakurei').util;
@@ -10,6 +18,7 @@ util.inherit(BlockGreen, base_object);
 
 BlockGreen.prototype.init = function(x, y) {
 	base_object.prototype.init.apply(this, arguments);
+	this.start_fall_frame = 0;
 	this.is_show = true;
 };
 
@@ -17,9 +26,21 @@ BlockGreen.prototype.spriteIndices = function(){
 	return [{x: 3, y: 0}];
 };
 
+BlockGreen.prototype.beforeDraw = function() {
+	base_object.prototype.beforeDraw.apply(this, arguments);
+	if(this.start_fall_frame && (FALL_SPAN - this.frame_count + this.start_fall_frame <= 0) ) {
+		this.is_show = false;
+
+		// reset
+		this.start_fall_frame = 0;
+	}
+};
+
+
+
 
 BlockGreen.prototype.fall = function(){
-	this.is_show = false;
+	this.start_fall_frame = this.frame_count;
 	this.is_collision = false;
 };
 
@@ -29,5 +50,28 @@ BlockGreen.prototype.isShow = function() {
 BlockGreen.prototype.isCollision = function() {
 	return this.is_collision;
 };
+
+BlockGreen.prototype.scaleWidth = function(){
+	var base_scale = base_object.prototype.scaleWidth.apply(this, arguments);
+	if (this.start_fall_frame) {
+		return(base_scale *  (FALL_SPAN - (this.frame_count - this.start_fall_frame)) / FALL_SPAN );
+	}
+	else {
+		return base_scale;
+	}
+};
+BlockGreen.prototype.scaleHeight = function(){
+	var base_scale = base_object.prototype.scaleHeight.apply(this, arguments);
+
+	if (this.start_fall_frame) {
+		return(base_scale *  (FALL_SPAN - (this.frame_count - this.start_fall_frame)) / FALL_SPAN );
+	}
+	else {
+		return base_scale;
+	}
+};
+
+
+
 
 module.exports = BlockGreen;
