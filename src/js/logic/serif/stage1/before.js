e6e2b4d0fe0e36cb7692db0fd67807ca48ad25c0
sairoutine <sairoutine@gmail.com>
2017-05-05 18:01:55 +0900

    fix: serif exp

--- src/js/logic/serif/stage1/before.js
+++ src/js/logic/serif/stage1/before.js
@@ -2,20 +2,20 @@
 
 // セリフ
 var Serif= [
-	{"pos":"left","exp":"normal","chara":"reimu","fukidashi":"normal","serif":"いたた..."},
+	{"pos":"left","exp":"normal1","chara":"reimu","fukidashi":"normal","serif":"いたた..."},
 	{"pos":"right","exp":"laugh","chara":"yukari","fukidashi":"normal","serif":"あらあら"},
-	{"pos":"left","exp":"normal","chara":"reimu","fukidashi":"normal","serif":"あ、ゆかり"},
-	{"pos":"left","exp":"angry","chara":"reimu","fukidashi":"normal","serif":"またアンタのしわざね"},
+	{"pos":"left","exp":"normal1","chara":"reimu","fukidashi":"normal","serif":"あ、ゆかり"},
+	{"pos":"left","exp":"angry1","chara":"reimu","fukidashi":"normal","serif":"またアンタのしわざね"},
 	{"pos":"right","exp":"laugh","chara":"yukari","fukidashi":"normal","serif":"暇そうにしてたから♪"},
 	{"pos":"right","exp":"laugh","chara":"yukari","fukidashi":"normal","serif":"それよりほら、見て"},
-	{"pos":"left","exp":"angry","chara":"reimu","fukidashi":"normal","serif":"なによ"},
-	{"pos":"left","exp":"normal","chara":"reimu","fukidashi":"normal","serif":"御札が落ちてるわ"},
+	{"pos":"left","exp":"angry1","chara":"reimu","fukidashi":"normal","serif":"なによ"},
+	{"pos":"left","exp":"normal1","chara":"reimu","fukidashi":"normal","serif":"御札が落ちてるわ"},
 	{"pos":"right","exp":"laugh","chara":"yukari","fukidashi":"normal","serif":"あれを全部拾うと帰れるわよ"},
-	{"pos":"left","exp":"normal","chara":"reimu","fukidashi":"normal","serif":"どういう理屈よ"},
-	{"pos":"left","exp":"normal","chara":"reimu","fukidashi":"normal","serif":"わたしじゃ取れない場所にあるじゃない"},
+	{"pos":"left","exp":"normal1","chara":"reimu","fukidashi":"normal","serif":"どういう理屈よ"},
+	{"pos":"left","exp":"normal1","chara":"reimu","fukidashi":"normal","serif":"わたしじゃ取れない場所にあるじゃない"},
 	{"pos":"right","exp":"laugh","chara":"yukari","fukidashi":"normal","serif":"手伝ってあげるわよ"},
 	{"pos":"right","exp":"laugh","chara":"yukari","fukidashi":"normal","serif":"Xボタンで私と霊夢の位置を入れかえることができるわ"},
 	{"pos":"right","exp":"laugh","chara":"yukari","fukidashi":"normal","serif":"私は霊夢と反対の位置にいるから"},
-	{"pos":"left","exp":"normal","chara":"reimu","fukidashi":"normal","serif":"Xボタンね"},
+	{"pos":"left","exp":"normal1","chara":"reimu","fukidashi":"normal","serif":"Xボタンね"},
 ];
 module.exports = Serif;
