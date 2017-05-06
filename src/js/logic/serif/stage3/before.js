18b9864490869fba9d50d6092ffa7058808825b6
sairoutine <sairoutine@gmail.com>
2017-05-06 22:04:16 +0900

    update: stage3 serif

--- src/js/logic/serif/stage3/before.js
+++ src/js/logic/serif/stage3/before.js
@@ -2,8 +2,9 @@
 
 // セリフ
 var Serif= [
-	{"pos":"right","exp":"normal1","chara":"reimu","fukidashi":"normal","serif":"なんか茶色いブロックがあるわ"},
-	{"pos":"left","exp":"normal1","chara":"yukari","fukidashi":"normal","serif":"一度乗ると消えちゃうみたいね"},
+	{"pos":"left","exp":"normal1","chara":"yukari","fukidashi":"normal","serif":null},
+	{"pos":"right","exp":"normal2","chara":"reimu","fukidashi":"normal","serif":"茶色のブロックがあるわ"},
+	{"pos":"left","exp":"normal1","chara":"yukari","fukidashi":"normal","serif":"一度乗ったら消えちゃうみたいね"},
 	{"pos":"right","exp":"normal1","chara":"reimu","fukidashi":"normal","serif":"消えちゃうのね"},
 ];
 module.exports = Serif;
