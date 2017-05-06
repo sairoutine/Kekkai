e57f0358da275c1b0065234baccb55e1e342a210
sairoutine <sairoutine@gmail.com>
2017-05-06 21:38:56 +0900

    update: stage2 serif

--- src/js/logic/serif/stage2/before.js
+++ src/js/logic/serif/stage2/before.js
@@ -2,14 +2,15 @@
 
 // セリフ
 var Serif= [
-	{"pos":"right","exp":"normal1","chara":"reimu","fukidashi":"normal","serif":"..."},
-	{"pos":"right","exp":"angry1","chara":"reimu","fukidashi":"normal","serif":"帰れてないじゃない！"},
-	{"pos":"left","exp":"normal1","chara":"yukari","fukidashi":"normal","serif":"まだ先があるみたいね"},
-	{"pos":"left","exp":"normal1","chara":"yukari","fukidashi":"normal","serif":"敵みたいなのがいるから気をつけて"},
+	{"pos":"left","exp":"ecstasy2","chara":"yukari","fukidashi":"normal","serif": "きゃー、れいむー！"},
+	{"pos":"left","exp":"ecstasy2","chara":"yukari","fukidashi":"normal","serif": "オバケ、こわーい！"},
+	{"pos":"right","exp":"normal2","chara":"reimu","fukidashi":"normal","serif":"(ベシッ)"},
+	{"pos":"left","exp":"yarare","chara":"yukari","fukidashi":"normal","serif": "いたいっ！"},
+	{"pos":"right","exp":"normal2","chara":"reimu","fukidashi":"normal","serif":"あれ、なによ"},
+	{"pos":"left","exp":"normal1","chara":"yukari","fukidashi":"normal","serif":"オバケみたいね"},
+	{"pos":"left","exp":"normal1","chara":"yukari","fukidashi":"normal","serif":"触れちゃダメよ"},
 	{"pos":"right","exp":"normal1","chara":"reimu","fukidashi":"normal","serif":"はいはい"},
-	{"pos":"left","exp":"normal1","chara":"yukari","fukidashi":"normal","serif":"わたしは敵に触れても大丈夫なのよ"},
-	{"pos":"right","exp":"normal1","chara":"reimu","fukidashi":"normal","serif":"はいはい"},
-	{"pos":"left","exp":"normal1","chara":"yukari","fukidashi":"normal","serif":"霊夢はあのオバケみたいなのに触れちゃだめよ"},
-	{"pos":"right","exp":"angry1","chara":"reimu","fukidashi":"normal","serif":"アンタだけずるくない？！"},
+	{"pos":"left","exp":"normal1","chara":"yukari","fukidashi":"normal","serif":"わたしはオバケに触れても大丈夫なのよ"},
+	{"pos":"right","exp":"angry1","chara":"reimu","fukidashi":"normal","serif":"アンタだけずるくない！？"},
 ];
 module.exports = Serif;
