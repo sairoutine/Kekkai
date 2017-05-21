7a06942bd78247323fb9afabed72ff6f603e9bbb
sairoutine <sairoutine@gmail.com>
2017-05-21 17:12:37 +0900

    add: prologue serif

---
+++ src/js/logic/serif/prologue.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// セリフ
+var Serif= [
+	{"pos":"right","exp":"normal1","chara":"reimu","serif":"あー、ったく暑い！\nもう晩夏だっていうのに、どうしてこうも暑いのよ！"},
+	{"pos":"right","exp":"normal1","chara":"reimu","serif":"せめてあの氷精みたいに、傍にいるだけで\n温度を下げてくれるような冷気を帯びた奴がいれば良いのに……"},
+	{"pos":"left","exp":"normal1","chara":"yukari","serif":"あら、呼んだかしら？"},
+	{"pos":"right","exp":"normal1","chara":"reimu","serif":"アンタは呼んでない！"},
+	{"pos":"left","exp":"normal1","chara":"yukari","serif":"そう？ 私の手にかかれば、例えばこんな感じで\n貴女の背中をつつーっとなぞれば……"},
+	{"pos":"right","exp":"normal1","chara":"reimu","serif":"な、何してんのよ！？"},
+	{"pos":"left","exp":"normal1","chara":"yukari","serif":"いやー、こうすれば少しは涼しくなるかなーって"},
+	{"pos":"right","exp":"normal1","chara":"reimu","serif":"アンタのそれは冷気じゃなくて寒気。気色悪いからやめて！"},
+	{"pos":"left","exp":"normal1","chara":"yukari","serif":"相変わらずつれないのね。他にもとっておきの案を持ってきたって言うのに"},
+	{"pos":"right","exp":"normal1","chara":"reimu","serif":"……アンタが出してきた案の中で、今までロクな試しなかったじゃない"},
+	{"pos":"left","exp":"normal1","chara":"yukari","serif":"まぁまぁそう言わずに。今回は暑がりな霊夢のために、\nとびっきりのスペシャルコースを用意しといたから"},
+	{"pos":"left","exp":"normal1","chara":"yukari","serif":"それじゃ、早速行くわよ"},
+
+	{"pos":"right","exp":"normal1","chara":"reimu","serif":"行くって、一体何をしに行く気よ"},
+	{"pos":"left","exp":"normal1","chara":"yukari","serif":"決まっているじゃない、霊夢"},
+	{"pos":"left","exp":"normal1","chara":"yukari","serif":"納涼の定番、肝試しよ"},
+];
+module.exports = Serif;
