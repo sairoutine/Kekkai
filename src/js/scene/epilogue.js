706eb705231e4230a9ee3eadbdc193f237c3660f
sairoutine <sairoutine@gmail.com>
2017-06-15 00:06:58 +0900

    add: mock of epilogue scene

---
+++ src/js/scene/epilogue.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/* エピローグ画面 */
+
+var serif_script = require("../logic/serif/epilogue");
+
+
+var util = require('../hakurei').util;
+var CONSTANT = require('../constant');
+var base_scene = require('./serif_base');
+var StorageSave = require('../save');
+
+var ScenePrologue = function(game) {
+	base_scene.apply(this, arguments);
+};
+
+util.inherit(ScenePrologue, base_scene);
+
+// 立ち絵＆セリフ終了後
+ScenePrologue.prototype.notifySerifEnd = function() {
+	// Exステージクリア */
+	var save_data = StorageSave.load();
+	if(!save_data) {
+		save_data = new StorageSave();
+	}
+	save_data.setIsExStageCleared(true);
+	save_data.save();
+
+	// タイトル画面へ
+	this.core.changeScene("title");
+};
+
+// セリフスクリプト
+ScenePrologue.prototype.serifScript = function() {
+	return serif_script;
+};
+
+// 背景画像名
+ScenePrologue.prototype.background = function() {
+	return "shrine_noon";
+};
+
+module.exports = ScenePrologue;
