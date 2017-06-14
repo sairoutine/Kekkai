7eeca55ffe2e0752c137ae687646dbc5613f756a
sairoutine <sairoutine@gmail.com>
2017-06-15 00:08:03 +0900

    fix: cannot load module

--- src/js/scene/staffroll.js
+++ src/js/scene/staffroll.js
@@ -2,14 +2,23 @@
 
 /* スタッフロール画面 */
 
+var base_scene = require('../hakurei').scene.base;
 var util = require('../hakurei').util;
-var CONSTANT = require('../constant');
-var base_scene = require('./base');
 
-var SceneStaffroll = function(game) {
+var SceneStaffroll = function(core) {
 	base_scene.apply(this, arguments);
 };
-
 util.inherit(SceneStaffroll, base_scene);
+
+SceneStaffroll.prototype.init = function() {
+	base_scene.prototype.init.apply(this, arguments);
+};
+
+SceneStaffroll.prototype.beforeDraw = function() {
+	base_scene.prototype.beforeDraw.apply(this, arguments);
+};
+SceneStaffroll.prototype.draw = function(){
+};
+
 
 module.exports = SceneStaffroll;
