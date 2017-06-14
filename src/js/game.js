b22e54a427d4d6378f424841bc391a0ec4dfbd13
sairoutine <sairoutine@gmail.com>
2017-06-14 19:07:42 +0900

    add: all scene to game class

--- src/js/game.js
+++ src/js/game.js
@@ -3,12 +3,27 @@ var core = require('./hakurei').core;
 var util = require('./hakurei').util;
 var CONSTANT = require('./constant');
 
+// ローディング画面
 var SceneLoading = require('./scene/loading');
+// タイトル画面
 var SceneTitle = require('./scene/title');
+// 回想シーン画面
+var SceneReminiscence = require('./scene/reminiscence');
+// プロローグ画面
 var ScenePrologue = require('./scene/prologue');
 var SceneStage = require('./scene/stage');
-var SceneStageEnd = require('./scene/stage_end');
-var PreReleaseEnd = require('./scene/prerelease_end');
+// 通常ストーリー クリア後画面
+var SceneAfterNormal = require('./scene/after_normal');
+// Ex エピグラフ画面
+var SceneExEpigraph = require('./scene/ex_epigraph');
+// Ex プロローグ画面
+var SceneExPrologue = require('./scene/ex_prologue');
+// Exストーリー クリア後画面
+var SceneAfterEx = require('./scene/after_ex');
+// スタッフロール
+var SceneStaffroll = require('./scene/staffroll');
+// エピローグ
+var SceneEpilogue = require('./scene/epilogue');
 
 var Game = function(canvas) {
 	core.apply(this, arguments);
@@ -19,32 +34,16 @@ Game.prototype.init = function () {
 	core.prototype.init.apply(this, arguments);
 
 	this.addScene("loading", new SceneLoading(this));
-	this.addScene("title", new SceneTitle(this)); // タイトル画面
-
-	/*
-	this.addScene("reminiscence", new SceneReminiscence(this)); // 回想画面
-	*/
-
-	this.addScene("prologue", new ScenePrologue(this)); // プロローグ
-
+	this.addScene("title", new SceneTitle(this));
+	this.addScene("reminiscence", new SceneReminiscence(this));
+	this.addScene("prologue", new ScenePrologue(this));
 	this.addScene("stage", new SceneStage(this));
-	this.addScene("stage_end", new SceneStageEnd(this)); // ノーマルステージ目終了後の画面
-
-	/*
-	// Exプロローグ
-	// もし、この幻想郷が全てを受け入れるというのであれば
-	// この結末を受け入れない私もまた、受け入れる筈よ！
-	this.addScene("epigraph", new SceneEpigraph(this)); // エピグラフ画面(詩の一節)
+	this.addScene("after_normal", new SceneAfterNormal(this));
+	this.addScene("ex_epigraph", new SceneExEpigraph(this));
 	this.addScene("ex_prologue", new SceneExPrologue(this));
-	*/
-
-	/*
-	this.addScene("ex_stage_end", new SceneExStageEnd(this));
-	this.addScene("staff_roll", new SceneStaffRoll(this));
+	this.addScene("after_ex", new SceneAfterEx(this));
+	this.addScene("staffroll", new SceneStaffroll(this));
 	this.addScene("epilogue", new SceneEpilogue(this));
-	*/
-
-	this.addScene("prerelease_end", new PreReleaseEnd(this));
 
 	this.changeScene("loading");
 
