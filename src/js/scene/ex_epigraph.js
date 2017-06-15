5a5845d8b939fcb31b53683d793abedd983e35fc
sairoutine <sairoutine@gmail.com>
2017-06-16 00:27:55 +0900

    update: implement ex_epigraph scene

--- src/js/scene/ex_epigraph.js
+++ src/js/scene/ex_epigraph.js
@@ -2,9 +2,8 @@
 
 /* Ex エピグラフ画面 */
 
-// 黒い画面に文字だけで、
-// 「幻想郷は全てを受け入れるのよ。それはそれは残酷な話ですわ。」
-
+// メッセージ表示の遷移時間
+var RESULT_TRANSITION_COUNT = 240;
 
 var base_scene = require('../hakurei').scene.base;
 var util = require('../hakurei').util;
@@ -20,9 +19,51 @@ SceneExEpigraph.prototype.init = function() {
 
 SceneExEpigraph.prototype.beforeDraw = function() {
 	base_scene.prototype.beforeDraw.apply(this, arguments);
+
+	// メッセージ表示終了
+	if(this.isTransitionEnd()) {
+		this.core.changeScene("ex_prologue");
+	}
 };
 SceneExEpigraph.prototype.draw = function(){
+	var ctx = this.core.ctx;
+	// 背景
+	ctx.save();
+	ctx.fillStyle = 'black';
+	ctx.fillRect(0, 0, this.core.width, this.core.height);
+	ctx.restore();
+
+	// 文字の透過度
+	var alpha = 0.0;
+	if(this.frame_count < RESULT_TRANSITION_COUNT/3) {
+		alpha = this.frame_count / (RESULT_TRANSITION_COUNT/3);
+	}
+	else if (RESULT_TRANSITION_COUNT/3 <= this.frame_count && this.frame_count < RESULT_TRANSITION_COUNT*2/3) {
+		alpha = 1.0;
+	}
+	else if (RESULT_TRANSITION_COUNT*2/3 <= this.frame_count && this.frame_count < RESULT_TRANSITION_COUNT) {
+		alpha = (RESULT_TRANSITION_COUNT - this.frame_count) / (RESULT_TRANSITION_COUNT/3);
+	}
+
+	// 文字
+	var message1 = "幻想郷は全てを受け入れるのよ。";
+	var message2 = "それはそれは残酷な話ですわ。";
+
+	var x = this.core.width/2 - 250;
+
+	ctx.save();
+	ctx.globalAlpha = alpha;
+	ctx.fillStyle = 'rgb( 255, 255, 255 )';
+	ctx.textAlign = 'left';
+	ctx.font = "30px 'Migu'";
+	ctx.fillText(message1, x, this.core.height/2);
+	ctx.fillText(message2, x, this.core.height/2 + 35);
+	ctx.restore();
+
 };
 
+SceneExEpigraph.prototype.isTransitionEnd = function(){
+	return RESULT_TRANSITION_COUNT < this.frame_count;
+};
 
 module.exports = SceneExEpigraph;
