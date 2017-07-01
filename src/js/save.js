b861d0770b104b40990234b2b5660e8c0396c6f1
sairoutine <sairoutine@gmail.com>
2017-07-02 02:52:57 +0900

    update: save class

--- src/js/save.js
+++ src/js/save.js
@@ -7,12 +7,60 @@ var StorageSave = function(scene) {
 };
 util.inherit(StorageSave, base_class);
 
-StorageSave.prototype.setIsNormalStageCleared = function(flag){
-	this._data.is_normal_stage_cleared = flag;
+// 通常ストーリークリアしたことを設定
+StorageSave.prototype.clearNormalStage = function(){
+	this.set("is_normal_stage_cleared", true);
 };
+// 通常ストーリークリアしたか否かを取得
 StorageSave.prototype.getIsNormalStageCleared = function(){
-	return this._data.is_normal_stage_cleared;
+	return this.get("is_normal_stage_cleared");
 };
+
+// ステージ実績の一覧を取得
+StorageSave.prototype.getStageResultList = function(){
+	var list = this.get("stage_result_list");
+
+	if(!list) list = [];
+
+	return list;
+};
+
+// 対象のステージ実績を取得
+StorageSave.prototype.getStageResult = function(stage_no){
+	var list = this.getStageResultList();
+
+	return list[stage_no - 1];
+};
+
+// 対象のステージ実績を更新
+StorageSave.prototype.updateStageResult = function(stage_no, time, exchange_num){
+	stage_no -= 1; // 配列なので 0 から
+	var list = this.getStageResultList();
+
+	// 初期化
+	if(!list[stage_no]) {
+		list[stage_no] = {
+			time:         null, // クリア時刻
+			exchange_num: null, // 使用 交換回数
+		};
+	}
+
+	// クリア時刻が早ければ更新
+	if(list[stage_no].time === null || list[stage_no].time > time) {
+		list[stage_no].time = time;
+	}
+
+	// 交換回数が少なければ更新
+	if(list[stage_no].exchange_num === null || list[stage_no].exchange_num > exchange_num) {
+		list[stage_no].exchange_num = exchange_num;
+	}
+
+	// セーブ
+	this.set("stage_result_list", list);
+};
+
+
+
 
 
 module.exports = StorageSave;
