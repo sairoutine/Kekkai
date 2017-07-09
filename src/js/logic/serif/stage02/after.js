267a3caab45f26fb729fdc4e06a596237ed347fa
sairoutine <sairoutine@gmail.com>
2017-07-09 15:58:52 +0900

    delete: nonneed serif

--- src/js/logic/serif/stage02/after.js
+++ src/js/logic/serif/stage02/after.js
@@ -2,15 +2,5 @@
 
 // セリフ
 var Serif= [
-	{"pos":"left","exp":"ecstasy2","chara":"yukari","fukidashi":"normal","serif": "きゃー、れいむー！"},
-	{"pos":"left","exp":"ecstasy2","chara":"yukari","fukidashi":"normal","serif": "オバケ、こわーい！"},
-	{"pos":"right","exp":"normal2","chara":"reimu","fukidashi":"normal","serif":"(ベシッ)"},
-	{"pos":"left","exp":"yarare","chara":"yukari","fukidashi":"normal","serif": "いたいっ！"},
-	{"pos":"right","exp":"normal1","chara":"reimu","fukidashi":"normal","serif":"あれ、なによ"},
-	{"pos":"left","exp":"normal4","chara":"yukari","fukidashi":"normal","serif":"オバケみたいね"},
-	{"pos":"left","exp":"normal3","chara":"yukari","fukidashi":"normal","serif":"触れちゃダメよ"},
-	{"pos":"right","exp":"normal2","chara":"reimu","fukidashi":"normal","serif":"はいはい"},
-	{"pos":"left","exp":"smile","chara":"yukari","fukidashi":"normal","serif":"わたしはオバケに触れても大丈夫だから"},
-	{"pos":"right","exp":"angry1","chara":"reimu","fukidashi":"normal","serif":"アンタだけずるくない！？"},
 ];
 module.exports = Serif;
