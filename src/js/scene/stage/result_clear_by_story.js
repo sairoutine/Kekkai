fe831491305731dd1e1bb280ee405c3b7e3c88a9
sairoutine <sairoutine@gmail.com>
2017-07-18 01:06:02 +0900

    update: result clear by story class

--- src/js/scene/stage/result_clear_by_story.js
+++ src/js/scene/stage/result_clear_by_story.js
@@ -5,6 +5,7 @@
 var base_scene = require('./result_clear_by_select');
 var CONSTANT = require('../../hakurei').constant;
 var util = require('../../hakurei').util;
+var Message = require('../../logic/result_message/story');
 
 var SceneStageResultClear = function(core, parent) {
 	base_scene.apply(this, arguments);
@@ -16,5 +17,25 @@ util.inherit(SceneStageResultClear, base_scene);
 SceneStageResultClear.prototype.goToNextScene = function() {
 	this.parent.notifyResultClearEndByStory();
 };
+
+// メッセージ取得
+SceneStageResultClear.prototype.getMessage = function() {
+	var stage_no = this.parent.stage_no;
+
+	// ステージに該当するメッセージ一覧を取得
+	var message_list = Message[stage_no - 1];
+
+	// ランダムに1つ取得
+	var message = message_list[Math.floor(Math.random() * message_list.length)];
+
+	return {
+		chara: message[0],
+		message: message[1],
+	};
+};
+
+
+
+
 
 module.exports = SceneStageResultClear;
