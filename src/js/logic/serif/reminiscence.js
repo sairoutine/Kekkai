948ff7049c57cc80cf76a5fb368b9a9d0f0b2a63
sairoutine <sairoutine@gmail.com>
2017-07-23 00:48:55 +0900

    update: serif

--- src/js/logic/serif/reminiscence.js
+++ src/js/logic/serif/reminiscence.js
@@ -2,6 +2,14 @@
 
 // セリフ
 var Serif= [
-	{"pos":"right","exp":"normal1","chara":"reimu","serif":"回想シーンセリフ"},
+	{"pos":null,"exp":null,"chara":null,"serif":"もう、霊夢ったら。そう力任せにぐちゃっと書いちゃダメよ。\n霊符にならないじゃない"},
+	{"pos":null,"exp":null,"chara":null,"serif":"霊符はね、紙と咒文とが組み合わさって初めて効果を発揮するの。いい？\n書く時はこう、筆の向きに逆らわないようにしながら、すすーっと走らせて――"},
+	{"pos":null,"exp":null,"chara":null,"serif":"そんなこと言われたって難しいんだもん！"},
+	{"pos":null,"exp":null,"chara":null,"serif":"そうやってすぐにむくれて筆を投げ落とさないの。ほら、ちゃんと筆を握って。\nいつかは貴女も、この札を使わなきゃいけなくなる時が来るのよ？"},
+	{"pos":null,"exp":null,"chara":null,"serif":"……紫が書いて作り置きしとけばいいじゃん"},
+	{"pos":null,"exp":null,"chara":null,"serif":"私がいつまで霊夢のそばにいられるか分からないでしょ？"},
+	{"pos":null,"exp":null,"chara":null,"serif":"そんなことあり得るの？\n紫の方がずっと長生きするのに"},
+	{"pos":null,"exp":null,"chara":null,"serif":"さぁ、どうかしら"},
+	{"pos":null,"exp":null,"chara":null,"serif":"でもね、霊夢。幻想郷は全てを受け入れるのよ。\nそれはそれは残酷な話ですわ"},
 ];
 module.exports = Serif;
