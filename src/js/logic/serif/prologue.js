1dc088599d42fb0bb6fc6b1620676a4b6ea76441
sairoutine <sairoutine@gmail.com>
2017-07-23 00:53:20 +0900

    update: serif

--- src/js/logic/serif/prologue.js
+++ src/js/logic/serif/prologue.js
@@ -3,15 +3,15 @@
 // セリフ
 var Serif= [
 	{"pos":"right","exp":"normal1","chara":"reimu","serif":"あー、ったく暑い！\nもう晩夏だっていうのに、どうしてこうも暑いのよ！"},
-	{"pos":"right","exp":"normal1","chara":"reimu","serif":"せめてあの氷精みたいに、傍にいるだけで\n温度を下げてくれるような冷気を帯びた奴がいれば良いのに……"},
+	{"pos":"right","exp":"normal1","chara":"reimu","serif":"せめてあの氷精みたいに、そばにいるだけで\n温度を下げてくれるような冷気を帯びた奴がいれば良いのに……"},
 	{"pos":"left","exp":"normal1","chara":"yukari","serif":"あら、呼んだかしら？"},
 	{"pos":"right","exp":"normal1","chara":"reimu","serif":"アンタは呼んでない！"},
 	{"pos":"left","exp":"normal1","chara":"yukari","serif":"そう？ 私の手にかかれば、例えばこんな感じで\n貴女の背中をつつーっとなぞれば……"},
 	{"pos":"right","exp":"normal1","chara":"reimu","serif":"な、何してんのよ！？"},
 	{"pos":"left","exp":"normal1","chara":"yukari","serif":"いやー、こうすれば少しは涼しくなるかなーって"},
-	{"pos":"right","exp":"normal1","chara":"reimu","serif":"アンタのそれは冷気じゃなくて寒気。気色悪いからやめて！"},
-	{"pos":"left","exp":"normal1","chara":"yukari","serif":"相変わらずつれないのね。他にもとっておきの案を持ってきたって言うのに"},
-	{"pos":"right","exp":"normal1","chara":"reimu","serif":"……アンタが出してきた案の中で、今までロクな試しなかったじゃない"},
+	{"pos":"right","exp":"normal1","chara":"reimu","serif":"アンタのそれは冷気じゃなくて寒気！気色悪いからやめて！"},
+	{"pos":"left","exp":"normal1","chara":"yukari","serif":"っもう、相変わらずつれないのね。他にもとっておきの案を持ってきたって言うのに"},
+	{"pos":"right","exp":"normal1","chara":"reimu","serif":"……アンタが出してきた案の中で、今までロクなものがなかったじゃない"},
 	{"pos":"left","exp":"normal1","chara":"yukari","serif":"まぁまぁそう言わずに。今回は暑がりな霊夢のために、\nとびっきりのスペシャルコースを用意しといたから"},
 	{"pos":"left","exp":"normal1","chara":"yukari","serif":"それじゃ、早速行くわよ"},
 
