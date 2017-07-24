4a206ea6a4fe09b449236ac57c2fb509085afe00
sairoutine <sairoutine@gmail.com>
2017-07-25 01:08:26 +0900

    update: serif

--- src/js/logic/serif/stage28/before.js
+++ src/js/logic/serif/stage28/before.js
@@ -5,10 +5,10 @@ var Serif= [
 	{"pos":"left","exp":"normal1","chara":"yukari","serif":null},
 	{"pos":"right","exp":"normal1","chara":"reimu","serif":"ねぇ、あと何か所くらい残っているのかしら？"},
 	{"pos":"right","exp":"normal1","chara":"reimu","serif":"そろそろ疲れてきたんだけれども"},
-	{"pos":"left","exp":"normal1","chara":"yukari","serif":"そうねぇ、ここを含めてあと3つってところかしら"},
+	{"pos":"left","exp":"normal4","chara":"yukari","serif":"そうねぇ、ここを含めてあと3つってところかしら"},
 	{"pos":"right","exp":"normal1","chara":"reimu","serif":"はぁ……、早く帰って水風呂でさっぱりしたいわね"},
-	{"pos":"left","exp":"normal1","chara":"yukari","serif":"あら、久しぶりに一緒に入る？\n昔みたいに頭の先から爪先までくまなく洗ってあげるわ"},
-	{"pos":"right","exp":"normal1","chara":"reimu","serif":"ゼッタイに嫌"},
-	{"pos":"left","exp":"normal1","chara":"yukari","serif":"恥ずかしがらなくて良いのにー"},
+	{"pos":"left","exp":"smile","chara":"yukari","serif":"あら、久しぶりに一緒に入る？\n昔みたいに頭の先から爪先までくまなく洗ってあげるわ"},
+	{"pos":"right","exp":"angry1","chara":"reimu","serif":"ゼッタイに嫌"},
+	{"pos":"left","exp":"ecstasy1","chara":"yukari","serif":"恥ずかしがらなくて良いのにー"},
 ];
 module.exports = Serif;
--- src/js/logic/serif/stage30/before.js
+++ src/js/logic/serif/stage30/before.js
@@ -4,10 +4,10 @@
 var Serif= [
 	{"pos":"left","exp":"normal1","chara":"yukari","serif":null},
 	{"pos":"right","exp":"normal1","chara":"reimu","serif":"長かった肝試しも、残すところこれで最後ってところかしら"},
-	{"pos":"right","exp":"normal1","chara":"reimu","serif":"ええ、そうね"},
-	{"pos":"left","exp":"normal1","chara":"yukari","serif":"どう？　楽しかったでしょ？"},
-	{"pos":"right","exp":"normal1","chara":"reimu","serif":"全然"},
-	{"pos":"left","exp":"normal1","chara":"yukari","serif":"最後までつれないのねぇ"},
+	{"pos":"left","exp":"normal3","chara":"yukari","serif":"ええ、そうね"},
+	{"pos":"left","exp":"normal3","chara":"yukari","serif":"どう？　楽しかったでしょ？"},
+	{"pos":"right","exp":"normal2","chara":"reimu","serif":"全然"},
+	{"pos":"left","exp":"normal2","chara":"yukari","serif":"最後までつれないのねぇ"},
 ];
 
 module.exports = Serif;
