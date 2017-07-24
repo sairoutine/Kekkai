2bd1c538cd1c17bae4e8b4e67d403ca579560845
sairoutine <sairoutine@gmail.com>
2017-07-25 02:49:04 +0900

    update: serifs

--- src/js/logic/serif/stage39/before.js
+++ src/js/logic/serif/stage39/before.js
@@ -2,9 +2,9 @@
 
 // セリフ
 var Serif= [
-	{"pos":"right","exp":"normal1","chara":"reimu","serif":"そろそろ私も辛くなってきたわね。油断したら一瞬で意識を持っていかれそうだわ"},
-	{"pos":"right","exp":"normal1","chara":"reimu","serif":"そうなる前に早く見つけ出さないと……！"},
-	{"pos":"right","exp":"normal1","chara":"reimu","serif":"一体どこにいるのよ、紫！"},
+	{"pos":"right","exp":"normal2","chara":"reimu","serif":"そろそろ私も辛くなってきたわね。油断したら一瞬で意識を持っていかれそうだわ"},
+	{"pos":"right","exp":"normal2","chara":"reimu","serif":"そうなる前に早く見つけ出さないと……！"},
+	{"pos":"right","exp":"angry1","chara":"reimu","serif":"一体どこにいるのよ、紫！"},
 ];
 
 module.exports = Serif;
