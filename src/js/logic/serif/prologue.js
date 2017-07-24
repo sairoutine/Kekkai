ac60d5112c03f8760e1b1a5174a00de02be436cb
sairoutine <sairoutine@gmail.com>
2017-07-24 23:44:42 +0900

    update: prologue serif

--- src/js/logic/serif/prologue.js
+++ src/js/logic/serif/prologue.js
@@ -2,21 +2,21 @@
 
 // セリフ
 var Serif= [
-	{"pos":"right","exp":"normal1","chara":"reimu","serif":"あー、ったく暑い！\nもう晩夏だっていうのに、どうしてこうも暑いのよ！"},
-	{"pos":"right","exp":"normal1","chara":"reimu","serif":"せめてあの氷精みたいに、そばにいるだけで\n温度を下げてくれるような冷気を帯びた奴がいれば良いのに……"},
-	{"pos":"left","exp":"normal1","chara":"yukari","serif":"あら、呼んだかしら？"},
-	{"pos":"right","exp":"normal1","chara":"reimu","serif":"アンタは呼んでない！"},
+	{"pos":"right","exp":"angry1","chara":"reimu","serif":"あー、ったく暑い！\nもう晩夏だっていうのに、どうしてこうも暑いのよ！"},
+	{"pos":"right","exp":"normal2","chara":"reimu","serif":"せめてあの氷精みたいに、そばにいるだけで\n温度を下げてくれるような冷気を帯びた奴がいれば良いのに……"},
+	{"pos":"left","exp":"smile","chara":"yukari","serif":"あら、呼んだかしら？"},
+	{"pos":"right","exp":"angry2","chara":"reimu","serif":"アンタは呼んでない！"},
 	{"pos":"left","exp":"normal1","chara":"yukari","serif":"そう？ 私の手にかかれば、例えばこんな感じで\n貴女の背中をつつーっとなぞれば……"},
-	{"pos":"right","exp":"normal1","chara":"reimu","serif":"な、何してんのよ！？"},
-	{"pos":"left","exp":"normal1","chara":"yukari","serif":"いやー、こうすれば少しは涼しくなるかなーって"},
-	{"pos":"right","exp":"normal1","chara":"reimu","serif":"アンタのそれは冷気じゃなくて寒気！気色悪いからやめて！"},
-	{"pos":"left","exp":"normal1","chara":"yukari","serif":"っもう、相変わらずつれないのね。他にもとっておきの案を持ってきたって言うのに"},
+	{"pos":"right","exp":"angry2","chara":"reimu","serif":"な、何してんのよ！？"},
+	{"pos":"left","exp":"ecstasy2","chara":"yukari","serif":"いやー、こうすれば少しは涼しくなるかなーって"},
+	{"pos":"right","exp":"angry2","chara":"reimu","serif":"アンタのそれは冷気じゃなくて寒気！気色悪いからやめて！"},
+	{"pos":"left","exp":"disappointed","chara":"yukari","serif":"っもう、相変わらずつれないのね。他にもとっておきの案を持ってきたって言うのに"},
 	{"pos":"right","exp":"normal1","chara":"reimu","serif":"……アンタが出してきた案の中で、今までロクなものがなかったじゃない"},
-	{"pos":"left","exp":"normal1","chara":"yukari","serif":"まぁまぁそう言わずに。今回は暑がりな霊夢のために、\nとびっきりのスペシャルコースを用意しといたから"},
-	{"pos":"left","exp":"normal1","chara":"yukari","serif":"それじゃ、早速行くわよ"},
+	{"pos":"left","exp":"normal3","chara":"yukari","serif":"まぁまぁそう言わずに。今回は暑がりな霊夢のために、\nとびっきりのスペシャルコースを用意しといたから"},
+	{"pos":"left","exp":"smile","chara":"yukari","serif":"それじゃ、早速行くわよ"},
 
 	{"pos":"right","exp":"normal1","chara":"reimu","serif":"行くって、一体何をしに行く気よ"},
 	{"pos":"left","exp":"normal1","chara":"yukari","serif":"決まっているじゃない、霊夢"},
-	{"pos":"left","exp":"normal1","chara":"yukari","serif":"納涼の定番、肝試しよ"},
+	{"pos":"left","exp":"smile","chara":"yukari","serif":"納涼の定番、肝試しよ"},
 ];
 module.exports = Serif;
