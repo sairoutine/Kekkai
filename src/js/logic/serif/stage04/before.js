f61c2db630c5c02f286ca0bdcf645a19d9db5d8b
sairoutine <sairoutine@gmail.com>
2017-07-28 21:33:42 +0900

    update serif

--- src/js/logic/serif/stage04/before.js
+++ src/js/logic/serif/stage04/before.js
@@ -3,7 +3,7 @@
 // セリフ
 var Serif= [
 	{"pos":"left","exp":"normal1","chara":"yukari","fukidashi":"normal","serif":null},
-	{"pos":"right","exp":"normal2","chara":"reimu","fukidashi":"normal","serif":"茶色のブロックがあるわ"},
+	{"pos":"right","exp":"normal2","chara":"reimu","fukidashi":"normal","serif":"色の違うブロックがあるわ"},
 	{"pos":"left","exp":"normal1","chara":"yukari","fukidashi":"normal","serif":"一度乗ったら消えちゃうみたいね"},
 	{"pos":"right","exp":"normal1","chara":"reimu","fukidashi":"normal","serif":"消えちゃうのね"},
 ];
