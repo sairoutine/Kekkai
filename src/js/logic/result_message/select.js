608499ce124141d833fa0ce24720cd9c2c69ec15
sairoutine <sairoutine@gmail.com>
2017-08-03 00:20:52 +0900

    update: result_message of select scene

--- src/js/logic/result_message/select.js
+++ src/js/logic/result_message/select.js
@@ -2,15 +2,15 @@
 
 // 星3つ
 var messageS = [
-	["reimu", "smile", "やるじゃない"],
-	["yukari", "normal1", "あら、すてきね"],
+	["reimu", "smile", "やるじゃない！"],
+	["yukari", "smile", "あら、すてきね！"],
 	//["reimu", "smile", ""],
 ];
 
 // 星2つ
 var messageA = [
 	["reimu", "normal2", "まぁ、こんなところね"],
-	//["yukari", "smile", "星2つ メッセージB"],
+	["yukari", "normal3", "上出来ね"],
 	//["reimu", "smile", "星2つ メッセージC"],
 ];
 
