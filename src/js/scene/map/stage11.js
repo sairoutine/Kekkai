88a454766f0fb4eae5f869fc760c135b2875a416
sairoutine <sairoutine@gmail.com>
2017-08-02 22:14:01 +0900

    add: criteria score

--- src/js/scene/map/stage11.js
+++ src/js/scene/map/stage11.js
@@ -38,4 +38,6 @@ module.exports = {
 	map: map, // マップ
 	exchange_num: 1, // 位置移動上限回数
 	is_vertical: false, // 交代が垂直かどうか
+	criteria_time:         740,   // スコア計算用基準タイム
+	criteria_exchange_num: 1,     // スコア計算用基準 交換回数
 };
--- src/js/scene/map/stage18.js
+++ src/js/scene/map/stage18.js
@@ -40,4 +40,6 @@ module.exports = {
 	map: map, // マップ
 	exchange_num: 3, // 位置移動上限回数
 	is_vertical: false, // 交代が垂直かどうか
+	criteria_time:         1100,   // スコア計算用基準タイム
+	criteria_exchange_num: 2,     // スコア計算用基準 交換回数
 };
