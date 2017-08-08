efd84305754b90acace3416c91e7bcce1e3b884d
sairoutine <sairoutine@gmail.com>
2017-08-09 02:20:47 +0900

    update: serif

--- src/js/logic/serif/stage12/before.js
+++ src/js/logic/serif/stage12/before.js
@@ -2,10 +2,5 @@
 
 // セリフ
 var Serif= [
-	{"pos":"left","exp":"normal1","chara":"yukari","serif":null},
-	{"pos":"right","exp":"normal2","chara":"reimu","serif":"結構先が長いのね"},
-	{"pos":"left","exp":"normal1","chara":"yukari","serif":"少しは涼しくなったかしら"},
-	{"pos":"right","exp":"normal2","chara":"reimu","serif":"アンタたち妖怪のほうが、よっぽど怖いわね"},
-	{"pos":"left","exp":"normal2","chara":"yukari","serif":"つれないわねぇ..."},
 ];
 module.exports = Serif;
--- src/js/logic/serif/stage36/before.js
+++ src/js/logic/serif/stage36/before.js
@@ -5,7 +5,7 @@ var Serif= [
 	{"pos":"right","exp":"normal1","chara":"reimu","serif":"ようやく待ちに待ったスキマたちがお目見えね。\n紫がいる境界まで大分近づいてきたってことかしら"},
 	{"pos":"right","exp":"normal1","chara":"reimu","serif":"それにしても笑っちゃうわよね。この前の終わらない冬の時は\n紫が境界に穴を空けたのに、今はその逆で私が空けてるだなんて"},
 	{"pos":"right","exp":"normal1","chara":"reimu","serif":"紫は番人失格だなんて言ってたけれども、\n私もこんなことしてたんじゃ博麗の巫女失格よね"},
-	{"pos":"right","exp":"normal1","chara":"reimu","serif":"今なら少しだけ……アイツの気持ちも分からなくはないかな……"},
+	{"pos":"right","exp":"confused","chara":"reimu","serif":"今なら少しだけ……アイツの気持ちも分からなくはないかな……"},
 ];
 
 module.exports = Serif;
