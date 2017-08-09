55548b6f942dd7603f9645885afe4caee5f95d60
sairoutine <sairoutine@gmail.com>
2017-08-10 00:56:10 +0900

    update: serif

--- src/js/logic/serif/stage38/before.js
+++ src/js/logic/serif/stage38/before.js
@@ -2,7 +2,6 @@
 
 // セリフ
 var Serif= [
-	{"pos":"right","exp":"normal1","chara":"reimu","serif":"スキマが相当増えてきたわね。結界が綻ぶのも時間の問題かしらね"},
 	{"pos":"right","exp":"normal1","chara":"reimu","serif":"そろそろアイツが出てきても良い頃合いなのだけれども……どこにいるのかしら"},
 	{"pos":"right","exp":"normal1","chara":"reimu","serif":"まさかアイツもこの狭間に溶けてしまうなんてことないよね……？"},
 ];
