03d93681294cbec7821759415856c3ef124f9934
sairoutine <sairoutine@gmail.com>
2017-08-10 20:41:06 +0900

    update: serif

--- src/js/logic/serif/stage18/before.js
+++ src/js/logic/serif/stage18/before.js
@@ -5,7 +5,7 @@ var Serif= [
 	{"pos":"left","exp":"normal1","chara":"yukari","serif":null},
 	{"pos":"right","exp":"confused","chara":"reimu","serif":"答えてよ紫。ここは一体どこなの？\nアンタは私に、一体何をやらせようとしているのよ？"},
 	{"pos":"left","exp":"normal4","chara":"yukari","serif":"……そうね、前者の問いかけに関して言えば、\n霊夢が言う通り、ここはスキマ空間ということになるかしら"},
-	{"pos":"right","exp":"confused","chara":"reimu","serif":"やっぱりそうだったのね。どうりでおかしいと思ったわ"},
+	{"pos":"right","exp":"confused","chara":"reimu","serif":"やっぱりそうだったのね。なんかおかしいと思ったわ"},
 	{"pos":"right","exp":"confused","chara":"reimu","serif":"どうしてそう、大事なことを黙っていたのよ？"},
 
 	{"pos":"left","exp":"normal2","chara":"yukari","serif":"……好きで黙っていたわけじゃないわ"},
--- src/js/logic/serif/stage38/before.js
+++ src/js/logic/serif/stage38/before.js
@@ -2,7 +2,7 @@
 
 // セリフ
 var Serif= [
-	{"pos":"right","exp":"normal1","chara":"reimu","serif":"そろそろアイツが出てきても良い頃合いなのだけれども……どこにいるのかしら"},
+	{"pos":"right","exp":"normal1","chara":"reimu","serif":"大分進んだから、そろそろアイツが出てきても\n良い頃合いなのだけれども……どこにいるのかしら"},
 	{"pos":"right","exp":"normal1","chara":"reimu","serif":"まさかアイツもこの狭間に溶けてしまうなんてことないよね……？"},
 ];
 
