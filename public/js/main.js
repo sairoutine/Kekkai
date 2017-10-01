19d364fed055f7afb1579dee90dd613feea5f25f
sairoutine <sairoutine@gmail.com>
2017-10-01 18:37:13 +0900

    compile js

--- public/js/main.js
+++ public/js/main.js
@@ -1,99 +1,110 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
+var CONSTANT = require("./constant");
+
 var AssetsConfig = {};
+
+// 体験版用
 AssetsConfig.images = {
 	// タイトルロゴ
-	title:         "./image/title.png",
+	title:         "./image/trial/title.png",
 	// タイトル背景
-	title_bg:      "./image/title_bg.png",
-	title_bg_without_yukari:      "./image/title_bg_without_yukari.png",
+	title_bg:      "./image/trial/title_bg.png",
 
 	// メニュー
-	menu_story_start_off:    "./image/menu/menu_story_start_off.png",
-	menu_story_start_on:     "./image/menu/menu_story_start_on.png",
-	menu_ex_story_start_off: "./image/menu/menu_ex_story_start_off.png",
-	menu_ex_story_start_on:  "./image/menu/menu_ex_story_start_on.png",
-	menu_select_stage_off:   "./image/menu/menu_select_stage_off.png",
-	menu_select_stage_on:    "./image/menu/menu_select_stage_on.png",
-	menu_music_room_off:     "./image/menu/menu_music_room_off.png",
-	menu_music_room_on:      "./image/menu/menu_music_room_on.png",
-	menu_how_to_off:         "./image/menu/menu_how_to_off.png",
-	menu_how_to_on:          "./image/menu/menu_how_to_on.png",
-	menu_config_off:         "./image/menu/menu_config_off.png",
-	menu_config_on:          "./image/menu/menu_config_on.png",
+	menu_story_start_off:    "./image/trial/menu/menu_story_start_off.png",
+	menu_story_start_on:     "./image/trial/menu/menu_story_start_on.png",
+	menu_ex_story_start_off: "./image/trial/menu/menu_ex_story_start_off.png",
+	menu_ex_story_start_on:  "./image/trial/menu/menu_ex_story_start_on.png",
+	menu_select_stage_off:   "./image/trial/menu/menu_select_stage_off.png",
+	menu_select_stage_on:    "./image/trial/menu/menu_select_stage_on.png",
+	menu_music_room_off:     "./image/trial/menu/menu_music_room_off.png",
+	menu_music_room_on:      "./image/trial/menu/menu_music_room_on.png",
+	menu_how_to_off:         "./image/trial/menu/menu_how_to_off.png",
+	menu_how_to_on:          "./image/trial/menu/menu_how_to_on.png",
+	menu_config_off:         "./image/trial/menu/menu_config_off.png",
+	menu_config_on:          "./image/trial/menu/menu_config_on.png",
 
 
 	// 回想シーン背景
-	reminiscence1:   "./image/reminiscence1.png",
-	reminiscence2:   "./image/reminiscence2.jpg",
+	reminiscence1:   "./image/trial/reminiscence1.png",
+	reminiscence2:   "./image/trial/reminiscence2.jpg",
 
 	// プロローグ背景
-	shrine_noon:   "./image/shrine_noon.png",
+	shrine_noon:   "./image/trial/shrine_noon.png",
 
 	// ステージ画面背景
-	stage_bg01:      "./image/stage_bg01.png",
-	stage_bg02:      "./image/stage_bg02.png",
-	stage_bg03:      "./image/stage_bg03.png",
-	stage_bg04:      "./image/stage_bg04.png",
+	stage_bg01:      "./image/trial/stage_bg01.png",
+	stage_bg02:      "./image/trial/stage_bg02.png",
+	stage_bg03:      "./image/trial/stage_bg03.png",
+	stage_bg04:      "./image/trial/stage_bg04.png",
 
 	// ステージ画面ステージ背景
-	bg:            "./image/bg.png",
-	stage_tile_24: "./image/stage_tile_24.png",
-	stage_tile_32: "./image/stage_tile_32.png",
-	block:         "./image/block.png",
-	hashigo:       "./image/hashigo.png",
-	//tile_brown:  "./image/tile_brown.gif",
-	//tile_purple: "./image/tile_purple.gif",
-	tile_gray:   "./image/tile_gray.gif",
-	tile_red:    "./image/tile_red.gif",
+	bg:            "./image/trial/bg.png",
+	stage_tile_24: "./image/trial/stage_tile_24.png",
+	stage_tile_32: "./image/trial/stage_tile_32.png",
+	block:         "./image/trial/block.png",
+	hashigo:       "./image/trial/hashigo.png",
+	//tile_brown:  "./image/trial/tile_brown.gif",
+	//tile_purple: "./image/trial/tile_purple.gif",
+	tile_gray:   "./image/trial/tile_gray.gif",
+	tile_red:    "./image/trial/tile_red.gif",
 
-	mari_bg:       "./image/mari_bg.png",
-	// ノーマルステージクリア後背景
-	shrine_night:   "./image/shrine_night.png",
-
-	after_ex1:   "./image/after_ex1.png",
-	after_ex2:   "./image/after_ex2.png",
-	after_ex3:   "./image/after_ex3.jpg",
-	after_ex4:   "./image/after_ex4.png",
-
-	epilogue1:   "./image/epilogue1.png",
-	epilogue2:   "./image/epilogue2.png",
-	epilogue3:   "./image/epilogue3.jpg",
-
+	mari_bg:       "./image/trial/mari_bg.png",
 	// スコアの★
-	star_on:       "./image/star_on.png",
-	star_off:      "./image/star_off.png",
+	star_on:       "./image/trial/star_on.png",
+	star_off:      "./image/trial/star_off.png",
 
 	// カーソル
-	cursor:      "./image/right_arrow.png",
+	cursor:      "./image/trial/right_arrow.png",
 
-	reimu_angry1:   "./image/reimu/angry1.png",
-	reimu_angry2:   "./image/reimu/angry2.png",
-	reimu_confused: "./image/reimu/confused.png",
-	reimu_cry:      "./image/reimu/cry.png",
-	reimu_laugh:    "./image/reimu/laugh1.png",
-	reimu_laugh2:   "./image/reimu/laugh2.png",
-	reimu_normal1:  "./image/reimu/normal1.png",
-	reimu_normal2:  "./image/reimu/normal2.png",
-	reimu_smile:    "./image/reimu/smile.png",
-	reimu_yarare:   "./image/reimu/yarare.png",
-	reimu_strength1:   "./image/reimu/strength1.png",
-	reimu_strength2:   "./image/reimu/strength2.png",
+	reimu_angry1:   "./image/trial/reimu/angry1.png",
+	reimu_angry2:   "./image/trial/reimu/angry2.png",
+	reimu_confused: "./image/trial/reimu/confused.png",
+	reimu_cry:      "./image/trial/reimu/cry.png",
+	reimu_laugh:    "./image/trial/reimu/laugh1.png",
+	reimu_laugh2:   "./image/trial/reimu/laugh2.png",
+	reimu_normal1:  "./image/trial/reimu/normal1.png",
+	reimu_normal2:  "./image/trial/reimu/normal2.png",
+	reimu_smile:    "./image/trial/reimu/smile.png",
+	reimu_yarare:   "./image/trial/reimu/yarare.png",
+	reimu_strength1:   "./image/trial/reimu/strength1.png",
+	reimu_strength2:   "./image/trial/reimu/strength2.png",
 
-	yukari_angry:        "./image/yukari/angry.png",
-	yukari_confused:     "./image/yukari/confused.png",
-	yukari_disappointed: "./image/yukari/disappointed.png",
-	yukari_ecstasy1:     "./image/yukari/ecstasy1.png",
-	yukari_ecstasy2:     "./image/yukari/ecstasy2.png",
-	yukari_laugh:        "./image/yukari/laugh.png",
-	yukari_normal1:      "./image/yukari/normal1.png",
-	yukari_normal2:      "./image/yukari/normal2.png",
-	yukari_normal3:      "./image/yukari/normal3.png",
-	yukari_normal4:      "./image/yukari/normal4.png",
-	yukari_smile:        "./image/yukari/smile.png",
-	yukari_yarare:       "./image/yukari/yarare.png",
+	yukari_angry:        "./image/trial/yukari/angry.png",
+	yukari_confused:     "./image/trial/yukari/confused.png",
+	yukari_disappointed: "./image/trial/yukari/disappointed.png",
+	yukari_ecstasy1:     "./image/trial/yukari/ecstasy1.png",
+	yukari_ecstasy2:     "./image/trial/yukari/ecstasy2.png",
+	yukari_laugh:        "./image/trial/yukari/laugh.png",
+	yukari_normal1:      "./image/trial/yukari/normal1.png",
+	yukari_normal2:      "./image/trial/yukari/normal2.png",
+	yukari_normal3:      "./image/trial/yukari/normal3.png",
+	yukari_normal4:      "./image/trial/yukari/normal4.png",
+	yukari_smile:        "./image/trial/yukari/smile.png",
+	yukari_yarare:       "./image/trial/yukari/yarare.png",
 };
+
+
+// 画像(完成版)
+if (!CONSTANT.TRIAL) {
+	Object.assign(AssetsConfig.images, {
+		title_bg_without_yukari:      "./image/production/title_bg_without_yukari.png",
+
+		// ノーマルステージクリア後背景
+		shrine_night:   "./image/production/shrine_night.png",
+
+		after_ex1:   "./image/production/after_ex1.png",
+		after_ex2:   "./image/production/after_ex2.png",
+		after_ex3:   "./image/production/after_ex3.jpg",
+		after_ex4:   "./image/production/after_ex4.png",
+
+		epilogue1:   "./image/production/epilogue1.png",
+		epilogue2:   "./image/production/epilogue2.png",
+		epilogue3:   "./image/production/epilogue3.jpg",
+	});
+}
 
 // sound ファイルはogg と m4a の二種類を用意してください
 AssetsConfig.sounds = {
@@ -134,10 +145,12 @@ AssetsConfig.sounds = {
 		volume: 1.0,
 	},
 };
+
+// BGM(体験版)
 // bgm ファイルはogg と m4a の二種類を用意してください
 AssetsConfig.bgms = {
 	title: {
-		path: "./bgm/title",
+		path: "./bgm/trial/title",
 		loopStart: 0*60 + 10 + 0.312,
 		loopEnd: 0*60 + 51 + 0.562,
 		title: "永夜抄 ～ Eastern Night.",
@@ -146,7 +159,7 @@ AssetsConfig.bgms = {
 		is_ex:     false,
 	},
 	reminiscence: {
-		path: "./bgm/reminiscence",
+		path: "./bgm/trial/reminiscence",
 		loopStart: 0*60 + 13 + 0.220,
 		loopEnd: 1*60 + 22 + 0.373,
 		title: "無何有の郷",
@@ -155,7 +168,7 @@ AssetsConfig.bgms = {
 		is_ex:     false,
 	},
 	prologue: {
-		path: "./bgm/prologue",
+		path: "./bgm/trial/prologue",
 		loopStart: 0*60 + 0 + 0.000,
 		loopEnd: 0*60 + 52 + 0.881,
 		title: "おてんば恋娘",
@@ -165,7 +178,7 @@ AssetsConfig.bgms = {
 	},
 
 	stage_a: {
-		path: "./bgm/stage_a",
+		path: "./bgm/trial/stage_a",
 		loopStart: 0*60 + 29 + 0.143,
 		loopEnd: 1*60 + 51 + 0.429,
 		title: "春色小径",
@@ -174,7 +187,7 @@ AssetsConfig.bgms = {
 		is_ex:     false,
 	},
 	stage_b: {
-		path: "./bgm/stage_b",
+		path: "./bgm/trial/stage_b",
 		loopStart: 0*60 + 0 + 0.000,
 		loopEnd: 1*60 + 22 + 0.500,
 		title: "少女綺想曲",
@@ -182,80 +195,94 @@ AssetsConfig.bgms = {
 		is_normal: false,
 		is_ex:     false,
 	},
-	stage_c: {
-		path: "./bgm/stage_c",
-		loopStart: 0*60 + 14 + 0.746,
-		loopEnd: 1*60 + 53 + 0.898,
-		title: "夜が降りてくる",
-		message: "ステージ曲その3よ。かなりゆったりとして、不穏な感じがとても強い曲ね。",
-		is_normal: false,
-		is_ex:     false,
-	},
-	stage_d: {
-		path: "./bgm/stage_d",
-		loopStart: 0*60 + 29 + 0.189,
-		loopEnd: 1*60 + 47 + 0.027,
-		title: "二色蓮花蝶",
-		message: "ラストバトルにふさわしい、かなり勢いのあるアレンジね。\nパズルゲームっぽくないけれど、緊迫した感じがあるわ。",
-		is_normal: true,
-		is_ex:     false,
-	},
-	stage_e: {
-		path: "./bgm/stage_e",
-		loopStart: 0*60 + 0 + 0.000,
-		loopEnd: 2*60 + 37 + 0.297,
-		title: "ネクロファンタジア",
-		message: "こっちも、ネクロファンタジア同様、ラストバトルにふさわしい感じのアレンジね。\n紫を救いたい気持ちが曲に現れていると思うわ。",
-		is_normal: true,
-		is_ex:     false,
-	},
-	title_without_yukari: {
-		path: "./bgm/title_without_yukari",
-		loopStart: 0*60 + 0 + 0.000,
-		loopEnd: 0*60 + 51 + 0.562,
-		title: "永夜抄 ～ Eastern Night.",
-		message: "退廃的な感じの曲ね。",
-		is_normal: true,
-		is_ex:     false,
-	},
-	after_ex: {
-		path: "./bgm/after_ex",
-		loopStart: 0*60 + 0 + 0.000,
-		loopEnd: 1*60 + 9 + 0.153,
-		title: "Eternal Dream",
-		message: "別れの悲しい感じが出てるアレンジね。\nとにかく悲しくなるように、かなり音数を絞ったんですって。",
-		is_normal: false,
-		is_ex:     true,
-	},
-	staffroll: {
-		path: "./bgm/staffroll",
-		loopStart: 0*60 + 8 + 0.727,
-		loopEnd: 1*60 + 4 + 0.364,
-		title: "月見草",
-		message: "めでたしめでたしじゃないエンディング曲ね。後味が悪い暗い感じにしようと思ったら\n思ったより暗くならなかったらしいわ。",
-		is_normal: false,
-		is_ex:     true,
-	},
-	epilogue: {
-		path: "./bgm/epilogue",
-		loopStart: 0*60 + 19 + 0.831,
-		loopEnd: 1*60 + 8 + 0.664,
-		title: "紅より儚い永遠",
-		message: "すごくハッピーエンドな曲ね。落ち着いためでたしめでたしな感じだわ。\n曲制作者曰く「やっぱり、ゆかれいむはハッピーエンドが似合いますね」ですって。",
-		is_normal: false,
-		is_ex:     true,
-	},
 };
+
+// BGM(完成版)
+if (!CONSTANT.TRIAL) {
+	Object.assign(AssetsConfig.bgms, {
+		stage_c: {
+			path: "./bgm/production/stage_c",
+			loopStart: 0*60 + 14 + 0.746,
+			loopEnd: 1*60 + 53 + 0.898,
+			title: "夜が降りてくる",
+			message: "ステージ曲その3よ。かなりゆったりとして、不穏な感じがとても強い曲ね。",
+			is_normal: false,
+			is_ex:     false,
+		},
+		stage_d: {
+			path: "./bgm/production/stage_d",
+			loopStart: 0*60 + 29 + 0.189,
+			loopEnd: 1*60 + 47 + 0.027,
+			title: "二色蓮花蝶",
+			message: "ラストバトルにふさわしい、かなり勢いのあるアレンジね。\nパズルゲームっぽくないけれど、緊迫した感じがあるわ。",
+			is_normal: true,
+			is_ex:     false,
+		},
+		stage_e: {
+			path: "./bgm/production/stage_e",
+			loopStart: 0*60 + 0 + 0.000,
+			loopEnd: 2*60 + 37 + 0.297,
+			title: "ネクロファンタジア",
+			message: "こっちも、ネクロファンタジア同様、ラストバトルにふさわしい感じのアレンジね。\n紫を救いたい気持ちが曲に現れていると思うわ。",
+			is_normal: true,
+			is_ex:     false,
+		},
+		title_without_yukari: {
+			path: "./bgm/production/title_without_yukari",
+			loopStart: 0*60 + 0 + 0.000,
+			loopEnd: 0*60 + 51 + 0.562,
+			title: "永夜抄 ～ Eastern Night.",
+			message: "退廃的な感じの曲ね。",
+			is_normal: true,
+			is_ex:     false,
+		},
+		after_ex: {
+			path: "./bgm/production/after_ex",
+			loopStart: 0*60 + 0 + 0.000,
+			loopEnd: 1*60 + 9 + 0.153,
+			title: "Eternal Dream",
+			message: "別れの悲しい感じが出てるアレンジね。\nとにかく悲しくなるように、かなり音数を絞ったんですって。",
+			is_normal: false,
+			is_ex:     true,
+		},
+		staffroll: {
+			path: "./bgm/production/staffroll",
+			loopStart: 0*60 + 8 + 0.727,
+			loopEnd: 1*60 + 4 + 0.364,
+			title: "月見草",
+			message: "めでたしめでたしじゃないエンディング曲ね。後味が悪い暗い感じにしようと思ったら\n思ったより暗くならなかったらしいわ。",
+			is_normal: false,
+			is_ex:     true,
+		},
+		epilogue: {
+			path: "./bgm/production/epilogue",
+			loopStart: 0*60 + 19 + 0.831,
+			loopEnd: 1*60 + 8 + 0.664,
+			title: "紅より儚い永遠",
+			message: "すごくハッピーエンドな曲ね。落ち着いためでたしめでたしな感じだわ。\n曲制作者曰く「やっぱり、ゆかれいむはハッピーエンドが似合いますね」ですって。",
+			is_normal: false,
+			is_ex:     true,
+		},
+	});
+}
+
+
+
 
 
 module.exports = AssetsConfig;
 
-},{}],2:[function(require,module,exports){
+},{"./constant":2}],2:[function(require,module,exports){
 'use strict';
 var DEBUG = require("./debug_constant");
 
 var CONSTANT = {
 	DEBUG: {},
+
+	// 体験版かどうか
+	TRIAL: false,
+	// 体験版の最終ステージ
+	TRIAL_LAST_STAGE_NO: 10,
 
 	// スコアの最高数
 	MAX_SCORE:  3,
@@ -339,9 +366,9 @@ module.exports = CONSTANT;
 },{"./debug_constant":3}],3:[function(require,module,exports){
 'use strict';
 var DEBUG = {
-	ON: true,
+	ON: false,
 	SOUND_OFF: true,
-	START_SCENE: "epilogue",
+	START_SCENE: "title",
 };
 
 module.exports = DEBUG;
@@ -15092,7 +15119,14 @@ SceneStage.prototype.notifyAfterTalkEnd = function() {
 		// 次のシーンへ
 		this.core.changeScene("after_ex");
 	}
-	// 次のステージへ
+	// 体験版終了ならば
+	else if (CONSTANT.TRIAL && this.stage_no === CONSTANT.TRIAL_LAST_STAGE_NO) {
+		// 進捗をリセット
+		this.core.storage_story.resetNormalStageProgress();
+
+		// タイトルへ戻る
+		this.core.changeScene("title");
+	}
 	else {
 		this.core.changeScene("stage", this.stage_no + 1);
 	}
@@ -19857,6 +19891,17 @@ StorageStory.prototype.clearExStageForDebug = function(){
 
 	this.save();
 };
+
+StorageStory.prototype.save = function(){
+	// 体験版はセーブ無効
+	if (CONSTANT.TRIAL) return;
+
+	base_class.prototype.save.apply(this, arguments);
+};
+
+
+
+
 
 module.exports = StorageStory;
 
